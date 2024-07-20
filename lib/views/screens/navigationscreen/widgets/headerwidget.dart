b0b24fb451a5ac6a44861b299bfba81200595a3e
brianmwirigi <bmmaingi17@gmail.com>
2024-07-20 15:17:42 +0300

    finalize header section

--- lib/views/screens/navigationscreen/widgets/headerwidget.dart
+++ lib/views/screens/navigationscreen/widgets/headerwidget.dart
@@ -5,19 +5,98 @@ class HeaderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       //code to make the header responsive using media query
       width: MediaQuery.of(context).size.width,
       height:
-          MediaQuery.of(context).size.height * 0.2, //20% of the screen height
+          MediaQuery.of(context).size.height * 0.3, //30% of the screen height
 
       child: Stack(
         children: [
-          Icon(
-            Icons.search,
-            weight: MediaQuery.of(context).size.width,
-            //  fit: BoxFit.cover,
+          Image.asset(
+            'assets/images/mukenaLogo.jpeg',
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.1,
+            fit: BoxFit.scaleDown,
           ),
+          Positioned(
+            left: 10,
+            top: 100,
+            right: 150,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: 50,
+              child: TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Search product',
+                  hintStyle: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  contentPadding: const EdgeInsets.all(10),
+                  prefixIcon: const Icon(Icons.search),
+                  suffixIcon: const Icon(Icons.camera_alt_outlined),
+                  fillColor: Colors.white,
+                  filled: true,
+                  focusColor: Colors.black,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 290,
+            top: 100,
+            child: Material(
+              type: MaterialType.transparency,
+              child: InkWell(
+                  overlayColor: WidgetStateProperty.all(Colors.red),
+                  onTap: () {},
+                  child: Ink(
+                    decoration: const BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Icon(
+                        Icons.notifications,
+                        color: Colors.red,
+                      ),
+                    ),
+                  )),
+            ),
+          ),
+          Positioned(
+              left: 350,
+              top: 100,
+              child: Material(
+                type: MaterialType.transparency,
+                child: InkWell(
+                    overlayColor: WidgetStateProperty.all(Colors.red),
+                    onTap: () {},
+                    child: Ink(
+                      decoration: const BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Icon(
+                          Icons.message,
+                          color: Colors.red,
+                        ),
+                      ),
+                    )),
+              ))
         ],
       ),
     );
