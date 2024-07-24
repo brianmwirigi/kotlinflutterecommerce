e9a1f94e4dadedabc6bad20da69f5a94e0fcfaab
brianmwirigi <bmmaingi17@gmail.com>
2024-07-24 17:25:29 +0300

    banner state management

--- lib/views/screens/navigationscreen/widgets/bannerwidget.dart
+++ lib/views/screens/navigationscreen/widgets/bannerwidget.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:kotlinflutterecommerce/controllers/bannercontroller.dart';
 
 class BannerWidget extends StatefulWidget {
   const BannerWidget({super.key});
@@ -9,41 +11,64 @@ class BannerWidget extends StatefulWidget {
 }
 
 class _BannerWidgetState extends State<BannerWidget> {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  final List _bannerImage = [];
-
-  getBanner() {
-    return _firestore
-        .collection('banner')
-        .get()
-        .then((QuerySnapshot querySnapshot) {
-      querySnapshot.docs.forEach((result) {
-        setState(() {
-          _bannerImage.add(result['image']);
-        });
-      });
-    });
-  }
-
-  @override
-  void initState() {
-    getBanner();
-    // TODO: implement initState
-    super.initState();
-  }
+  final BannerController _bannerController = BannerController();
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 100,
-      width: MediaQuery.of(context).size.width,
-      child: PageView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: _bannerImage.length,
-        itemBuilder: (context, index) {
-          return Image.network(_bannerImage[index]);
-        },
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height * 0.2,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.red.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 5.0,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: StreamBuilder<List<String>>(
+          stream: _bannerController.getBannerUrls(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.red,
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return const Icon(Icons.error);
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Center(
+                child: Text(
+                  'No data found',
+                  style: TextStyle(fontSize: 18, color: Colors.grey),
+                ),
+              );
+            } else {
+              return Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  PageView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.network(snapshot.data![index],
+                            fit: BoxFit.contain),
+                      );
+                    },
+                  )
+                ],
+              );
+            }
+          },
+        ),
       ),
     );
   }
